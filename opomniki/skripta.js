d91ced3e90f05e9d795d1f53843f6af8deeb5c45
Emir Muzaferovic <em1315@student.uni-lj.si>
2016-03-08 07:33:04 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,5 +1,14 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	
+	var izvediPrijavo = function() {
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+		
+	}
+		
+	document.querySelector("#prijavniGumb").addEventListener("click", izvediPrijavo);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
